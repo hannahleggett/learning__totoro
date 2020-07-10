cbbee0567a5d8d65172f2a72f36545ec8fbbed87
Hannah Leggett <hannahleggett@MacBook-Pro.broadband>
2020-07-10 10:52:13 +0100

    reorganise folders

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo "My Neighbour Totoro";
-?>
+
+<head>
+    <title>Totoro</title>
+    <link href="inc/CSS/style.css" rel="stylesheet" type="text/css"></link>
+    <link href="inc/JS/app.js" rel="script" type="text/javasript"></link>
+</head>
+
+<body>
+    <article class="Container">
+
+        <?php 
+        include 'views/header.php';
+        ?>
+
+        <main>
+
+        </main>
+
+        <?php 
+        include 'views/footer.php';
+        ?>
+
+    </article>
+</body>
+
---
+++ views/header.php
@@ -0,0 +1,7 @@
+<header>
+    <ul>
+        <li>Totoro</li>
+        <li>Spirited Away</li>
+        <li>Cat Returns</li>
+    </ul>
+</header>
